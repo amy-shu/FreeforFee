ebc966fe920181cde4868aa816625a7527e2ea88
root <pav920@gmail.com>
2015-01-17 20:18:28 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, url_for
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/post', methods=['POST'])
+def test():
+    print request.form
+    print request.method
+    a = request.form['a']
+    print a
+    return a
+
+@app.route('/random/<rString>')
+def displayString(rString):
+    origin = "asdasd"
+    destination = "Sasdssssssasd"
+    return render_template('map2.html',
+        origin = origin,
+        destination = destination)
---
+++ freeforfee.wsgi
@@ -0,0 +1,5 @@
+import sys
+sys.path.insert(0, '/var/www/html/freeforfee')
+
+from app import app as application
+
